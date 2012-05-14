312c6d85a3b50499c5b1159e1313bef2db507bc8
Jack Dempsey <jack.dempsey@gmail.com>
2012-05-14 14:36:34 -0700

    use rspec's generators

--- lib/rspec_full_engine/engine.rb
+++ lib/rspec_full_engine/engine.rb
@@ -1,4 +1,13 @@
 module RspecFullEngine
   class Engine < ::Rails::Engine
+    # RSpec's default Rails test generators simply don't work with engines (they don't namespace stuff correctly and they don't
+    # know how to handle engine route helpers).
+    #
+    # Nevertheless, you may want to enable them purely for the creation of the
+    # correct files, which you can modify after generation to work correctly.
+    config.generators do |g|                                                               
+      g.test_framework :rspec
+      g.integration_tool :rspec
+    end
   end
 end
